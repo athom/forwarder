eca229a9b1127df0f647d721d01aac45e9924e08
athom <athom@126.com>
2016-08-17 14:25:42 +0800

    add options

--- forwarder.go
+++ forwarder.go
@@ -1,14 +1,31 @@
 package forwarder
 
 import (
+	"bytes"
 	"compress/gzip"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 )
 
+var Debug = false
+var UseGZip = true
+
 func Forward(w http.ResponseWriter, r *http.Request, targetURL string) (err error) {
-	request, err := http.NewRequest(r.Method, targetURL, r.Body)
+	var buf io.Reader
+	buf = r.Body
+	if Debug {
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return err
+		}
+		log.Println(`curl -d '` + string(b) + `' ` + targetURL)
+		buf = bytes.NewBuffer(b)
+	}
+
+	request, err := http.NewRequest(r.Method, targetURL, buf)
 	if err != nil {
 		return
 	}
@@ -22,14 +39,17 @@ func Forward(w http.ResponseWriter, r *http.Request, targetURL string) (err erro
 
 	defer res.Body.Close()
 
-	str := r.Header.Get(`Accept-Encoding`)
 	rd := res.Body
-	if strings.Contains(str, `gzip`) {
-		rd, err = gzip.NewReader(res.Body)
-		if err != nil { // 请求不要求gzip编码
-			return
+	if UseGZip {
+		str := r.Header.Get(`Accept-Encoding`)
+		if strings.Contains(str, `gzip`) {
+			rd, err = gzip.NewReader(res.Body)
+			if err != nil { // 请求不要求gzip编码
+				return
+			}
 		}
 	}
+
 	_, err = io.Copy(w, rd)
 	if err != nil {
 		return
